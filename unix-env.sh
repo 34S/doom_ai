8aa2cfa4ed116d0006945c50006f36351c53cc5c
Colin Ray <r.colinray@iastate.edu>
2013-11-06 18:01:13 -0600

    Initial commit of chocolate-doom source files.

---
+++ unix-env.sh
@@ -0,0 +1,6 @@
+#!/bin/bash
+
+# DOOM WADs should be placed in DOOMWADDIR. Alternatively, you may choose to 
+# pass "-wad path/to/wad" as an argument to chocolate-doom. See src/d_iwad.c for
+# platform-specific search paths, as well as recognized WAD filenames.
+export DOOMWADDIR=~/.chocolate-doom
